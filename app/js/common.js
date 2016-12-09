9fee7828ecb5efc5a684714db72f140f756e7d87
KudelAndrei <kudel1995@mail.ru>
2016-12-09 23:51:02 +0300

    section all

--- app/js/common.js
+++ app/js/common.js
@@ -3,7 +3,20 @@ $(document).ready(function() {
 	// ВЫравнивание блоков по высоте
 	//$(".service-item h4").equalHeights(); 
 
-	//Слайдер  https://gist.github.com/KudelAndrei/fca3e6e73e1e0aa21996a12af58c9069
+	//плавный скролл
+	$(".menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+		top = $(id).offset().top;
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1000 );
+
+	});
+
+	//Слайдер 
 	$(".sl-header").slick({
 		arrows: true,
 		dots: true,
